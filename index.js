1718c4e79faed066b5406776b47c3330b0111925
Cavan <cavan@radarrelay.com>
2020-01-07 10:12:38 -0700

    Update example for 0x v3

--- index.js
+++ index.js
@@ -4,48 +4,50 @@ const WebSocketClient = require('websocket').client;
 const client = new WebSocketClient({tlsOptions: {rejectUnauthorized: false}});
 let lastPing = new Date().getTime();
 
-client.on('connectFailed', function(error) {
+client.on('connectFailed', error => {
     console.log('Connect Error: ' + error.toString());
 });
 
-client.on('connect', function(connection) {
+client.on('connect', connection => {
     console.log('Connected to Server...');
-    connection.on('error', function(error) {
+    connection.on('error', (error) => {
         console.log("Connection Error: " + error.toString());
     });
-    connection.on('close', function() {
+    connection.on('close', () => {
         console.log('Connection Closed');
     });
-    connection.on('message', function(message) {
+    connection.on('message', message => {
       if (message.type === 'utf8') {
         console.log(message.utf8Data);
       }
     });
-    connection.on('pong', function(){
+    connection.on('pong', () => {
       console.log('[pingpong] response took', (new Date().getTime() - lastPing) + 'ms');
     })
 
-    function send(message) {
+    const send = message => {
       if (connection.connected) {
-          connection.sendUTF(message);
+        connection.sendUTF(message);
       }
-    }
+    };
 
     // subscribe to book
-    send(`{
-      "type": "SUBSCRIBE",
-      "topic": "BOOK",
-      "market": "ZRX-WETH", // e.g. 'ZRX-WETH'
-      "requestId": "1"
-    }`);
+    send(
+      JSON.stringify({
+        type: 'SUBSCRIBE',
+        topic: 'BOOK',
+        market: 'ZRX-WETH', // e.g. 'ZRX-WETH'
+        requestId: '1'
+      })
+    );
 
     // Send a ping every 10s to
     // keep the connection live
-    setInterval(function(){
+    setInterval(() => {
       lastPing = new Date().getTime();
       connection.ping();
     }, 10000);
 
 });
 
-client.connect('wss://ws.radarrelay.com/v2');
+client.connect('wss://ws.radarrelay.com/v3');
